b2368bb1f30e390cac26e10266ac7a7b777e1c4c
Ivis Perdomo <iperdomo@ivyper.dev>
2021-02-12 00:00:00 -0500

    feat(main): add input validation loop for user entry - Wrap user prompt in a loop to catch non-letter inputs with try/except - Continue prompting until valid alphabetic word is provided - Convert input to uppercase and exit loop on successful entry

--- main.py
+++ main.py
@@ -14,12 +14,22 @@ data = pd.read_csv('./nato_phonetic_alphabet.csv')
 # Build a dictionary mapping each uppercase letter to its NATO code word.
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-# Prompt the user for a word and convert it to uppercase to match dictionary keys.
-user_input = input("Enter a word: ").upper()
+# Create a loop that continues until a valid input is given.
+mapping = True
 
-# Translate each letter in the input to its code word, if it exists in the dictionary.
-# Non-alphabet characters are simply skipped.
-phonetic_output = [phonetic_dict[letter] for letter in list(user_input) if letter in phonetic_dict]
+while mapping:
+    # Use error-handling to not break the program with an error when an invalid input is given.
+    try:
+        # Prompt the user for a word and convert it to uppercase to match dictionary keys.
+        user_input = input("Enter a word: ").upper()
 
-# Display the list of NATO code words.
-print(phonetic_output)
+        # Translate each letter in the input to its code word, if it exists in the dictionary.
+        # Non-alphabet characters are simply skipped.
+        phonetic_output = [phonetic_dict[letter] for letter in list(user_input)]
+
+        # Display the list of NATO code words.
+        print(phonetic_output)
+        # Exit the loop if a valid input is given and no exceptions occur.
+        break
+    except KeyError:
+        print("Sorry, only letters in the alphabet are allowed.")
